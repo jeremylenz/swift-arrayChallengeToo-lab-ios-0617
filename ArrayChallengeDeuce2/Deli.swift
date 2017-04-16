453f0d61025428a3008e8bdc91b9d03edc9d390e
jeremylenz <jeremylenz@gmail.com>
2017-04-15 20:55:40 -0400

    all tests good

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,18 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+        }
         
+        if line.count == 1 {
+        return "Welcome \(name), you're first in line!"
+        } else {
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
     }
     
     
@@ -25,14 +36,29 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            let lastName = line.removeFirst()
+            return "Now serving \(lastName)!"
+        }
+        
+        
     }
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+         var returnString = "The line is:"
+            for (index, name) in line.enumerated() {
+                returnString.append("\n\(index + 1). \(name)")
+            }
+            return returnString
+        }
 
     }
     
